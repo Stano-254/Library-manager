6ea9c34545e4032edaa658c6c0cc65f403a14717
Stanley <stanley.alucheri@spinmobile.co.ke>
2023-06-16 21:36:54 +0300

    create member

--- members/administration/members_administration.py
+++ members/administration/members_administration.py
@@ -1,16 +1,49 @@
-class MembersAdministration(object):
+import logging
+
+from base.backend.transactionlogbase import TransactionLogBase
+from base.backend.utils.utilities import validate_name
+from members.backend.service import MemberService
+from django.forms.models import model_to_dict
+
+lgr = logging.getLogger(__name__)
+
+
+class MembersAdministration(TransactionLogBase):
     """
     handle the administration functionality relating to members including CRUD
     """
 
-    def create_member(self, request, kwargs):
+    def create_member(self, request, **kwargs):
         """
           Handles adding of members in the library system
           :param request: the original request
           :param kwargs: keyword arguments for creation of member.
           :return: dict response with code
           """
-        pass
+        transaction = None
+        try:
+            transaction = self.log_transaction('CreateMember', request=request, user=request.user)
+            if not transaction:
+                return {'code': '900.500.500', 'message': 'Create member transaction failed'}
+            first_name = kwargs.get('first_name')
+            last_name = kwargs.get('last_name')
+            if not validate_name(first_name):
+                self.mark_transaction_failed(transaction, message="Invalid first name")
+                return {'code': '500.400.003', 'message': 'Invalid first name'}
+            if not validate_name(last_name):
+                self.mark_transaction_failed(transaction, message="Invalid last name")
+                return {'code': '500.400.003', 'message': 'Invalid last name'}
+            member = MemberService().create(**kwargs)
+            if not member:
+                self.mark_transaction_failed(
+                    transaction, message='Failed to create member', response_code='200.001.003')
+                return {'code': '200.001.003', 'message': 'Failed to create member'}
+            self.complete_transaction(transaction, message='Success')
+            return {'code': '100.000.000', 'message': 'success', 'data': model_to_dict(member)}
+        except Exception as e:
+            lgr.exception(f"Exception occurred during create of member :{e}")
+            self.mark_transaction_failed(transaction, response=str(e))
+            return {'code': '999.999.999', 'message': 'Error occurred during creation of member'}
 
     def get_member(self, request, member_id):
         """
